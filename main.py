6d599b57cac1afafd21b961a07c81ab545a7e5f8
Arisha <79106539+Arina842@users.noreply.github.com>
2023-11-08 13:16:46 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+import math
+import numpy as np
+
+x_list = [1, 2, 3, 4]
+y_list = [1, 2, 3, 4]
+s = map(lambda x, y: math.sqrt(x**2+y**2), x_list, y_list)
+print(list(s))
+
+arr_x = np.square(x_list)
+print(arr_x)
+
+b = [x**2 for x in x_list]
+print(b)
